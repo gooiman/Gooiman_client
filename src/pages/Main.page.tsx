5b009e7c1557ac50b933bcbc89c05365dbd101cf
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-09-28 15:05:16 +0900

    feat: Sidebar UI 열고 닫기 기능 구현 (#3)
    * chore: SidebarClosed SVG 파일 추가
    
    * feat: Sidebar 열고 닫기 기능 구현

--- src/pages/Main.page.tsx
+++ src/pages/Main.page.tsx
@@ -1,18 +1,47 @@
-import Styled from '@emotion/styled';
+import styled from '@emotion/styled';
+import { useState } from 'react';
 
+import SVGSidebarClosed from '../assets/SidebarClosed.svg?react';
 import Sidebar from '@/components/Sidebar';
 
 const Main = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen((prev) => !prev);
+  };
+
   return (
-    <MainContainer>
-      <Sidebar />
-      MainPage
-    </MainContainer>
+    <>
+      <SidebarContainer>
+        <Sidebar isOpen={isSidebarOpen} onToggle={toggleSidebar} />
+      </SidebarContainer>
+      <MainContainer isSidebarOpen={isSidebarOpen}>
+        <SVGSideMenuContainer isSidebarOpen={isSidebarOpen} onClick={toggleSidebar}>
+          <SVGSidebarClosed style={{ width: '25px', height: '25px', transform: 'scale(-1, 1)', cursor: 'pointer' }} />
+        </SVGSideMenuContainer>
+        <Content></Content>
+      </MainContainer>
+    </>
   );
 };
 
-const MainContainer = Styled.div`
+const MainContainer = styled.div<{ isSidebarOpen: boolean }>`
   display: flex;
+  padding: 20px;
+  margin-left: ${(props) => (props.isSidebarOpen ? '190px' : '0')};
+  transition: margin-left 0.3s ease;
 `;
+
+const SVGSideMenuContainer = styled.div<{ isSidebarOpen: boolean }>`
+  display: flex;
+  position: absolute;
+  opacity: ${(props) => (props.isSidebarOpen ? 0 : 1)};
+  transition: opacity 0.3s ease;
+`;
+
+const SidebarContainer = styled.div``;
+
+const Content = styled.div``;
 
 export default Main;
