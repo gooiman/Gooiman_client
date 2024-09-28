d273c9e97259de80e548d9341bfcec016d35c6d1
TaegeonKim <aksen5240@gmail.com>
2024-09-28 15:27:58 +0900

    feat: 사이드바 기본 디자인 레이아웃 제작

--- src/components/modal/CustomModal.tsx
+++ src/components/modal/CustomModal.tsx
@@ -1,6 +1,7 @@
 import { useModalStore } from '@/store/useModalStore';
 import Modal from 'react-modal';
 import Styled from '@emotion/styled';
+import SVGBlueCloud from '@/assets/BlueCloud.svg?react';
 
 const CreateMemo = ({ modalId }: { modalId: string }) => {
   const { modals, closeModal } = useModalStore();
@@ -14,11 +15,30 @@ const CreateMemo = ({ modalId }: { modalId: string }) => {
       isOpen={modals === modalId}
       onRequestClose={closeCreateModal}
       contentLabel="Create Memo"
-      ariaHideApp={false} // 스크린 리더를 위한 설정
-      closeTimeoutMS={300} // 모달 닫힐 때 애니메이션 시간
+      ariaHideApp={false} 
+      closeTimeoutMS={300} 
     >
       <ModalContent>
-        <h2>Create Memo</h2>
+        <HeaderText>새 메모 제목</HeaderText>
+        <Underline />
+        <TopicContainer>
+          <TopicContent>
+            <SVGBlueCloud />
+            <TextWrapper>대주제</TextWrapper>
+            <TextWrapper>주제 선택</TextWrapper>
+          </TopicContent>
+          <TopicContent>
+            <SVGBlueCloud />
+            <TextWrapper>소주제</TextWrapper>
+            <TextWrapper>주제 선택</TextWrapper>
+          </TopicContent>
+        </TopicContainer>
+
+        {/* 텍스트 입력 가능한 input 박스 */}
+        <InputContainer>
+          <StyledInput placeholder="메모 내용을 입력하세요..." />
+        </InputContainer>
+
         <button onClick={closeCreateModal}>Close Modal</button>
       </ModalContent>
     </SidebarModal>
@@ -31,7 +51,8 @@ const SidebarModal = Styled(Modal)`
     display: flex;
     justify-content: flex-end; 
     align-items: flex-start;
-    transition: opacity 200ms ease-in-out; 
+    transition: opacity 200ms ease-in-out;
+    height: 100vh;
   }
 
   &.ReactModal__Content {
@@ -40,48 +61,86 @@ const SidebarModal = Styled(Modal)`
     right: 0;
     bottom: 0;
     width: 50%;
+    height: 100vh; 
     background-color: #ffffff;
-    padding: 60px 0px;
-    overflow-y: auto;
-    box-shadow: 0 0 15px rgba(0, 0, 0, 0.3); 
+    display: flex;
+    flex-direction: column;
+    box-shadow: 0 0 15px rgba(0, 0, 0, 0.3);
     transform: translateX(100%); 
-    transition: transform 200ms ease-in-out;
+    transition: transform 300ms ease-in-out;
 
-  
     &.ReactModal__Content--after-open {
       transform: translateX(0); 
     }
 
     &.ReactModal__Content--before-close {
-      transform: translateX(100%); 
+      transform: translateX(100%);
     }
   }
 `;
 
 const ModalContent = Styled.div`
-  h2 {
-    margin-top: 0;
-    font-size: 24px;
-  }
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 60px 48px;
+  height: 100%;
+`;
 
-  button {
-    background-color: #007bff;
-    color: white;
-    border: none;
-    padding: 10px 20px;
-    font-size: 14px;
-    cursor: pointer;
-    border-radius: 5px;
-    margin-top: 20px;
-    transition: background-color 0.3s ease;
+const HeaderText = Styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 8px;
+  font-size: 2.5rem;
+  color: #4d4d4d;
+`;
 
-    &:hover {
-      background-color: #0056b3;
-    }
+const Underline = Styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: #e0e0e0;
+  margin-bottom: 28px;
+`;
 
-    &:focus {
-      outline: none;
-    }
+const TopicContainer = Styled.div`
+  display: flex;
+  margin-bottom: 45px;
+  flex-direction: column;
+`;
+
+const TopicContent = Styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 16px;
+`;
+
+const TextWrapper = Styled.div`
+  font-size: 2rem;
+`;
+
+// Input 필드 스타일링
+const InputContainer = Styled.div`
+  flex: 1;  
+  display: flex;
+  flex-direction: column;
+  padding: 0 0 12px;
+`;
+
+const StyledInput = Styled.textarea`
+  flex: 1;  
+  padding: 12px;
+  font-size: 1.2rem;
+  border: 1px solid #e0e0e0;
+  border-radius: 25px;
+  outline: none;
+  resize: none;  
+  
+
+  &:focus {
+    border-color: #007bff;
+    box-shadow: 0 0 0 2px rgba(0, 123, 255, 0.25);
   }
 `;
 
