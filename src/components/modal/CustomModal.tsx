8b9e72f6970e2510357b66dc716a553b035032cc
TaegeonKim <aksen5240@gmail.com>
2024-09-28 15:54:43 +0900

    feat: 메모 제목 및 메모 글자수 제한 설정

--- src/components/modal/CustomModal.tsx
+++ src/components/modal/CustomModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useModalStore } from '@/store/useModalStore';
 import Modal from 'react-modal';
 import Styled from '@emotion/styled';
@@ -5,6 +6,19 @@ import SVGBlueCloud from '@/assets/BlueCloud.svg?react';
 
 const CreateMemo = ({ modalId }: { modalId: string }) => {
   const { modals, closeModal } = useModalStore();
+
+  const [title, setTitle] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+  const [charCount, setCharCount] = useState(0);
+  const maxChars = 150;
+
+  const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const input = e.target.value;
+    if (input.length <= maxChars) {
+      setContent(input);
+      setCharCount(input.length);
+    }
+  };
 
   const closeCreateModal = () => {
     closeModal(modalId);
@@ -15,12 +29,13 @@ const CreateMemo = ({ modalId }: { modalId: string }) => {
       isOpen={modals === modalId}
       onRequestClose={closeCreateModal}
       contentLabel="Create Memo"
-      ariaHideApp={false} 
-      closeTimeoutMS={300} 
+      ariaHideApp={false}
+      closeTimeoutMS={300}
     >
       <ModalContent>
-        <HeaderText>새 메모 제목</HeaderText>
+        <TitleInput type="text" placeholder="새 메모 제목" value={title} onChange={(e) => setTitle(e.target.value)} />
         <Underline />
+
         <TopicContainer>
           <TopicContent>
             <SVGBlueCloud />
@@ -34,18 +49,19 @@ const CreateMemo = ({ modalId }: { modalId: string }) => {
           </TopicContent>
         </TopicContainer>
 
-        {/* 텍스트 입력 가능한 input 박스 */}
+        {/* 메모 내용 입력 필드 */}
         <InputContainer>
-          <StyledInput placeholder="메모 내용을 입력하세요..." />
+          <StyledInput placeholder="메모 내용을 입력하세요..." value={content} onChange={handleContentChange} />
+          <CharCounter>
+            {charCount}/{maxChars}자
+          </CharCounter>
         </InputContainer>
-
-        <button onClick={closeCreateModal}>Close Modal</button>
+        <ButtonContainer onClick={closeCreateModal}>Save</ButtonContainer>
       </ModalContent>
     </SidebarModal>
   );
 };
 
-// 모달의 스타일을 사이드바 형식으로 조정
 const SidebarModal = Styled(Modal)`
   &.ReactModal__Overlay {
     display: flex;
@@ -61,6 +77,7 @@ const SidebarModal = Styled(Modal)`
     right: 0;
     bottom: 0;
     width: 50%;
+    min-width: 400px;
     height: 100vh; 
     background-color: #ffffff;
     display: flex;
@@ -87,13 +104,16 @@ const ModalContent = Styled.div`
   height: 100%;
 `;
 
-const HeaderText = Styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 8px;
+const TitleInput = Styled.input`
   font-size: 2.5rem;
+  padding: 12px;
+  border: none;
+  border-radius: 5px;
+  margin-bottom: 8px;
+  width: 100%;
   color: #4d4d4d;
+  font-family: 'BMHANNA';
+  outline: none;
 `;
 
 const Underline = Styled.div`
@@ -120,28 +140,54 @@ const TextWrapper = Styled.div`
   font-size: 2rem;
 `;
 
-// Input 필드 스타일링
 const InputContainer = Styled.div`
   flex: 1;  
   display: flex;
   flex-direction: column;
+  position: relative;
   padding: 0 0 12px;
 `;
 
 const StyledInput = Styled.textarea`
   flex: 1;  
-  padding: 12px;
+  padding: 16px;
   font-size: 1.2rem;
-  border: 1px solid #e0e0e0;
+  border: 1px solid #82AFFF;
   border-radius: 25px;
   outline: none;
   resize: none;  
-  
+  font-family: 'BMHANNA';
 
   &:focus {
-    border-color: #007bff;
     box-shadow: 0 0 0 2px rgba(0, 123, 255, 0.25);
   }
+`;
+
+const CharCounter = Styled.div`
+  position: absolute;
+  bottom: 24px;
+  right: 16px;
+  font-size: 0.9rem;
+  color: #666;
+  padding: 2px 5px;
+  border-radius: 4px;
+`;
+
+const ButtonContainer = Styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  font-family: 'PressStart2P';
+  padding: 14px 76px;
+  background-color: #82AFFF;
+  border: none;
+  color: #ffffff;
+  border-radius: 5px;
+  cursor: pointer;
+  margin-top: 12px;
+  font-size: 1.2rem;
+  outline: none;
 `;
 
 export default CreateMemo;
