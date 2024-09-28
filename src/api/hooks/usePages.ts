caeaf6d3102b07de60dda021375d0eeb99458d3a
TaegeonKim <aksen5240@gmail.com>
2024-09-29 06:06:54 +0900

    feat: API 엔드포인트 수정

--- src/api/hooks/usePages.ts
+++ src/api/hooks/usePages.ts
@@ -5,14 +5,14 @@ import { useQuery } from '@tanstack/react-query';
 // 페이지 정보를 가져오는 API 호출 함수
 const getPageInfo = async (pageId: string) => {
   const { data } = await apiClient.get(API_ENDPOINTS.PAGE.GET_PAGE.replace(':pageId', pageId));
-  return data.data;
+  return data;
 };
 
 // React Query의 useQuery 훅을 이용해 데이터를 가져옴
 export const usePageInfo = (pageId: string) => {
   return useQuery({
-    queryKey: ['pageInfo', pageId], 
-    queryFn: () => getPageInfo(pageId), 
-    enabled: !!pageId, 
+    queryKey: ['pageInfo', pageId],
+    queryFn: () => getPageInfo(pageId),
+    enabled: !!pageId,
   });
 };
